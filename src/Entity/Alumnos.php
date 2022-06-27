7ec33356a144ca36bd06a4d25bcf5706413c026a
Ignacio Lopez <Nacho.ios@icloud.com>
2022-06-27 20:05:21 -0300

    first commit

---
+++ src/Entity/Alumnos.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Alumnos
+ *
+ * @ORM\Table(name="alumnos")
+ * @ORM\Entity
+ */
+class Alumnos
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=60, nullable=false)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido", type="string", length=60, nullable=false)
+     */
+    private $apellido;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="edad", type="integer", nullable=false)
+     */
+    private $edad;
+
+    //Getter y Setter
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido(): ?string
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido(string $apellido): self
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+
+}
