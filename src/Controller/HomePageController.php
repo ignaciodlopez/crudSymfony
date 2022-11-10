806cb5d414bd5d02fddf796f818c44482f7a3001
Ignacio Lopez <Nacho.ios@icloud.com>
2022-11-10 18:40:26 -0300

    Update

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     /**
-     * @Route("/home", name="home_page")
+     * @Route("/", name="home_page")
      */
     public function index(): Response
     {
